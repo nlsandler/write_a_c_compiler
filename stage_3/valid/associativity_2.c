23c6d0f21e0976064db3309b2d0d3bc732ecd0e7
Nora Sandler <nlsandler@gmail.com>
2017-12-14 16:10:06 -0500

    stage 3 tests

---
+++ stage_3/valid/associativity_2.c
@@ -0,0 +1,3 @@
+int main() {
+    return 6 / 3 / 2;
+}
--- test_compiler.sh
+++ test_compiler.sh
@@ -10,7 +10,7 @@ cmp=$1
 success_total=0
 failure_total=0
 
-num_stages=2
+num_stages=3
 for i in `seq 1 $num_stages`; do
     success=0
     fail=0
